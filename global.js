64f4ac283f9f5a80e2d5d6cb3d0b5a6c28d44cb3
andrewyounucsd <asyoun@ucsd.edu>
2026-04-16 18:16:55 -0700

    made dropdown functional

--- global.js
+++ global.js
@@ -48,3 +48,15 @@ nav.insertAdjacentHTML(
     </select>
   </label>`,
 );
+
+let select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event) {
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+if ('colorScheme' in localStorage) {
+  document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
