4699ba117355248cc2041f754c568e35333024c0
andrewyounucsd <asyoun@ucsd.edu>
2026-04-16 18:03:20 -0700

    added selector to js

--- global.js
+++ global.js
@@ -36,3 +36,15 @@ for (let p of pages) {
     a.classList.add('current');
   }
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `<label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
