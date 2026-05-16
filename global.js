f6b8e60304def18653095fd9ca6f84c966610b9a
andrewyounucsd <asyoun@ucsd.edu>
2026-05-15 20:03:19 -0700

    added project to projects page and added url function

--- global.js
+++ global.js
@@ -96,10 +96,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
   for (let project of projects) {
     const article = document.createElement('article');
     article.innerHTML = `
-      <${headingLevel}>${project.title}</${headingLevel}>
+      <${headingLevel}>${project.url ? `<a href="${project.url}" target="_blank">${project.title}</a>` : project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
       <div>
-       <p>${project.description}</p>
+        <p>${project.description}</p>
         <p class="project-year">${project.year}</p>
       </div>
     `;
