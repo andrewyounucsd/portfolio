9ed902322f5b8ca63396913cbbff4a116e82a324
andrewyounucsd <asyoun@ucsd.edu>
2026-05-22 15:14:18 -0700

    fixed import problem

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,5 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
 
 async function loadData() {
   const data = await d3.csv('loc.csv', (row) => ({
